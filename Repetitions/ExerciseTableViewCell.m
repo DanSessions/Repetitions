1c4dadd1c79c70cc973bfb2be9eed0bc58dfa8d6
Dan Sessions <dansessions@gmail.com>
2014-04-24 23:40:05 +0100

    Removed template code.

--- Repetitions/ExerciseTableViewCell.m
+++ Repetitions/ExerciseTableViewCell.m
@@ -10,25 +10,9 @@
 
 @implementation ExerciseTableViewCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
-    }
-    return self;
-}
-
-- (void)awakeFromNib
-{
-    // Initialization code
-}
-
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
